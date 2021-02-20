0af209da84a0424ca8d79aefa0633a5e715eebf7
Darrekt <jkricklau@gmail.com>
2021-02-20 16:45:26 +0000

    1 line data cleaning

--- src/regression_methods/regression.py
+++ src/regression_methods/regression.py
@@ -1,3 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
 
-def testfunc():
-    print("Hello regression")
+data = pd.read_csv("src/priceData.csv")
+px = data.iloc[:, 1:]
+px = px.interpolate()
+
+logpx = np.log(px)
+logpx.rolling(252).mean().plot()
+plt.show()
